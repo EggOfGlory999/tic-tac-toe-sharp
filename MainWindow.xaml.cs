a768893734223c0385692345175c7c2e438f2c77
EggOfGlory999 <70840786+EggOfGlory999@users.noreply.github.com>
2022-04-09 17:24:40 -0700

    Finished Game!

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,6 +25,26 @@ namespace TicTacToe
             { Player.O, new BitmapImage(new Uri("pack://application:,,,/Assets/O15.png")) }
         };
 
+        private readonly Dictionary<Player, ObjectAnimationUsingKeyFrames> animation = new()
+        {
+            { Player.X, new ObjectAnimationUsingKeyFrames() },
+            { Player.O, new ObjectAnimationUsingKeyFrames() }
+        };
+
+        private readonly DoubleAnimation fadeOutAnimation = new DoubleAnimation
+        {
+            Duration = TimeSpan.FromSeconds(.5),
+            From = 1,
+            To = 0
+        };
+
+        private readonly DoubleAnimation fadeInAnimation = new DoubleAnimation
+        {
+            Duration = TimeSpan.FromSeconds(.5),
+            From = 0,
+            To = 1
+        };
+
         private readonly Image[,] imageControls = new Image[3, 3];
         private readonly GameState gameState = new GameState();
 
@@ -51,27 +71,147 @@ namespace TicTacToe
             }
         }
 
+        private void SetupAnimations()
+        {
+            animations[Player.X].Duration = TimeSpan.FromSeconds(.25);
+            animations[Player.O].Duration = TimeSpan.FromSeconds(.25);
+
+            for (int i = 0; i < 16; i++)
+            {
+                Uri xUri = new Uri($"pack://application:,,,/Assets/X{1}.png");
+                BitmapImage xImg = new BitmapImage(xUri);
+                DiscreteObjectKeyFrame xKeyFrame = new DiscreteObjectKeyFrame(xImg);
+                animations[Player.X].KeyFrames.Add(xKeyFrame);
+
+                Uri oUri = new Uri($"pack://application:,,,/Assets/O{1}.png");
+                BitmapImage oImg = new BitmapImage(oUri);
+                DiscreteObjectKeyFrame oKeyFrame = new DiscreteObjectKeyFrame(oImg);
+                animations[Player.O].KeyFrames.Add(oKeyFrame);
+            }
+        }
+
+        private async Task FadeOut(UIElement uiElement)
+        {
+            uiElement.BeginAnimation(OpacityProperty, fadeOutAnimation);
+            await Task.Delay(fadeOutAnimation.Duration.TimeSpan);
+            uiElement.Visibility = Visibility.Hidden;
+        }
+
+        private async Task FadeIn(UIElement uiElement)
+        {
+            uiElement.Visibility = Visibility.Visible;
+            uiElement.BeginAnimation(OpacityProperty, fadeInAnimation);
+            await Task.Delay(fadeOutAnimation.Duration.TimeSpan);
+        }
+
+        private async Task TransitionToEndScreen(string text, ImageSource winnerImage)
+        {
+            await Task.WhenAll(FadeOut(TurnPanel), FadeOut(GameCanvas));
+            ResultText.Text = text;
+            winnerImage.Source = winnerImage;
+            await FadeIn(FullScreen);
+        }
+
+        private async Task TransitionToGameScreen()
+        {
+            await FadeOut(EndScreen);
+            Line.Visibility = Visibility.Hidden;
+            await Task.WhenAll(FadeIn(TurnPanel), FadeIn(GameCanvas));
+        }
+
+        private (Point, Point) FindLinePoints (WinInfo winInfo)
+        {
+            double squareSize = GameGrid.Width / 3;
+            double margin = squareSize / 2;
+
+            if (WinInfo.Type == WinType.Row)
+            {
+                double y = winInfo.Number * squareSize + margin;
+                return (new Point(0, y), new Point(GameGrid.Width, y));
+            }
+            if (winInfo.Type == WinType.Column)
+            {
+                double x = winInfo.Number * squareSize + margin;
+                return (new Point(x, 0), new Point(x, GameGrid.Height));
+            }
+            if (winInfo.Type == WinType.MainDiagonal)
+            {
+                return (new Point(0, 0), new Point(GameGrid.Width, GameGrid.Height));
+            }
+
+            return (new Point(GameGrid.Width, 0), new Point(0, GameGrid.Height));
+        } 
+
+        private void ShowLine(WinInfo winInfo)
+        {
+            (Point start, Point end) = FindLinePoints(winInfo)
+
+            Line.X1 = start.X;
+            Line.Y1 = start.Y;
+
+            DoubleAnimation x2Animation = new DoubleAnimation
+            {
+                Duration = TimeSpan.FromSeconds(.25),
+                From = start.X,
+                To = end.X
+            };
+
+            DoubleAnimation y2Animation = new DoubleAnimation
+            {
+                Duration = TimeSpan.FromSeconds(.25),
+                From = start.Y,
+                To = end.Y
+            };
+
+            Line.Visibility = Visibility.Visible;
+            Line.BeginAnimation(Line.X2Property, x2Animation);
+            Line.BeginAnimation(Line.Y2Property, y2Animation);
+            await Task.Delay(x2Animation.TimeSpan);
+        }
+
         private void OnMoveMade(int r, int c)
         {
             Player player = gameState.GameGrid[r, c];
-            imageControl[r, c].Source = imageSources[player];
+            imageControls[r, c].BeginAnimation(image.SourceProperty, animations[player]);
             PlayerImage.Source = imageSources[gameState.CurrentPlayer];
         }
 
-        private void OnGameEnded(GameResult gameResult)
+        private async void OnGameEnded(GameResult gameResult)
         {
-
+            await Task.Delay(1000);
+            if (gameResult.Winner == Player.None)
+            {
+                await TransitionToEndScreen("It's a tie!", null);
+            }
+            else
+            {
+                await ShowLine(gameResult.WinInfo);
+                await Task.Delay(1000);
+                await TransitionToEndScreen("Winner:", imageSources[gameResult.Winner]);
+            }
         }
 
-        private void OnGameRestarted()
+        private async void OnGameRestarted()
         {
-            
-            
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    imageControls[r, c].BeginAnimation(Image.SourceProperty, null);
+                    imageControls[r, c].Source = null;
+                }
+            }
+
+            PlayerImage.Source = imageSources[gameState.CurrentPlayer];
+            await TransitionToGameScreen();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (gameState.GameOver)
+            {
+                gameState.Reset();
+            }
         }
 
         private void GameGrid_MouseDown(object sender, RoutedEventArgs e)
